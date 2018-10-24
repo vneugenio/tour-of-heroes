f0c97df1569edec6e232526369d3361e23cf0b24
bolts <voltaire.eugenio@gmail.com>
2018-10-25 03:29:03 +0800

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { Heroes } from './mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+    heroes = Heroes;
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    selectedHero: Hero;
+    onSelect(hero: Hero):void {
+        this.selectedHero = hero;
+    }
+
+}
